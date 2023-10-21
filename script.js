c070b8504713c95e26aa3603392c2355d9f8734b
MohamadhuAshik <mohamadashik268@gmail.com>
2023-10-21 19:29:35 +0530

    add

--- script.js
+++ script.js
@@ -79,7 +79,7 @@ function validate(){
         error6.textContent = "Password Not Match "
          return false
       }else{
-        alert(`${fname.value}  Register Successfully you Redirect on Login Page`)
+        alert(`${fname.value} Register Successfully you Redirect on Login Page`)
       } 
 }
 
@@ -114,6 +114,6 @@ function login(){
     error9.textContent = "Password Not Match"
     return false
   }else{
-    alert(`${Username.value}Login Successfully You Redirecting On Api-Data-Page`)
+    alert(`${Username.value} Login Successfully You Redirecting On Api-Data-Page`)
   }
 }
